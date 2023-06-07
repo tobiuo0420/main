2f42cec497c2c72a96c2e00e473ad24229898116
tobiuo0420 <133127590+tobiuo0420@users.noreply.github.com>
2023-06-08 01:24:34 +0900

    java課題 真偽値と条件式

--- HelloWorld_FamilynameFirstname/src/curriculum_A/Qes1_13.java
+++ HelloWorld_FamilynameFirstname/src/curriculum_A/Qes1_13.java
@@ -3,128 +3,379 @@ package curriculum_A;
 public class Qes1_13 {
 
 	public static void main(String[] args) {
+		//1,バイト型（byte）,短整数型（short）,整数型（int）,長整数型（long）
+		//単精度浮動小数点数型（float）,倍精度浮動小数点数型（double）
+		//文字型（char）,文字列型（String）,ブーリアン型（boolean)
+		
+		
+		// バイト型宣言
+		byte ten;
+		
+		// 短整数型宣言
+		short onehundred;
+		
+		// 整数型宣言
+		int onethousand;
+		
+		// 長整数型宣言
+		long tenthousand;
+		
+		// 単精度浮動小数点数型宣言
+		float f;
+		
+		// 倍精度浮動小数点数型宣言
+		double d;
+		
+		// 文字型宣言
+		char c;
+		
+		// 文字列型宣言
+		String str;
+		
+		// ブーリアン型宣言
+		boolean bool;
+		
+		
+		//2,それぞれのローカル変数をローカル内でそれぞれの初期値を代入し、
+		//初期化してください
 
-		// バイト型（byte） 1,2,3
-		byte b = 10;
-		System.out.println("バイト型：" + b);
+		// バイト型
+		ten = 0; // 初期化
 
-		// 短整数型（short）
-		short s = 100;
-		System.out.println("短整数型：" + s);
+		// 短整数型
+		onehundred = 0; // 初期化
 
-		// 整数型（int）
-		int i = 1000;
-		System.out.println("整数型：" + i);
+		// 整数型
+		onethousand = 0; // 初期化
 
-		// 長整数型（long）
-		long l = 10000L;
-		System.out.println("長整数型：" + l);
+		// 長整数型
+		tenthousand = 0L; // 初期化
+		
+		// 単精度浮動小数点数型
+		f = 0.0f; // 初期化
 
-		// 単精度浮動小数点数型（float）
-		float f = 9.5f;
+		// 倍精度浮動小数点数型
+		d = 0.0d; // 初期化
+		
+		// 文字型
+		c = '\u0000'; // 初期化
+		
+		// 文字列型
+		str = null; // 初期化
+
+		// ブーリアン型
+		bool = false; // 初期化
+
+		
+		//3,初期化をしたそれぞれの変数に下記の値を代入してください
+		
+		System.out.println("問3");
+		
+		
+		// バイト型
+		ten = 10; // tenに10を代入
+				
+		// 変数値を表示
+		System.out.println("バイト型：" + ten);
+
+		// 短整数型
+		onehundred = 100; // onehundredに100を代入
+		
+		// 変数値を表示
+		System.out.println("短整数型：" + onehundred);
+
+		// 整数値
+		onethousand = 1000; // onethousandに1000を代入
+		
+		// 変数値を表示
+		System.out.println("整数型：" + onethousand); 
+
+		// 長整数型
+		tenthousand = 10000L; // tenthousandに10000Lを代入
+		
+		// 変数値を表示
+		System.out.println("長整数型：" + tenthousand); 
+		
+		// 単精度浮動小数点数型
+		f = 9.5f; // fに9,5を代入
+		
+		// 変数の値を表示
 		System.out.println("単精度浮動小数点数型：" + f);
-
-		// 倍精度浮動小数点数型（double）
-		double d = 10.5d;
+		
+		// 倍精度浮動小数点数型
+		d = 10.5d; // dに10,5を代入
+				
+		// 変数の値を表示
 		System.out.println("倍精度浮動小数点数型：" + d);
 
-		// 文字型（char）
-		char c = 'a';
+		// 文字型
+		c = 'a'; // cにaを代入
+		
+		// 変数値を表示
 		System.out.println("文字型：" + c);
 
-		// 文字列型（String）
-		String str = "ハロー";
+		// 文字列型
+		str = "ハロー"; // strにハローを代入
+		
+		// 変数値を表示
 		System.out.println("文字列型：" + str);
 
-		// ブーリアン型（boolean）
-		boolean bool = true;
-		System.out.println("ブーリアン型：" + bool);
+		// ブーリアン型
+		bool = true; // boolにtrueを代入
+		
+		// 変数値を表示
+		System.out.println("ブーリアン型：" + bool + "\n");
 
-		//4
-		System.out.println(b + s + i + l);
+		
+		//4,下記の通りにコンソール出力されるようにしてください
+		//上記で作成した変数を必ず使用すること
+		
+		System.out.println("問4");
+
+		// 10足す100足す1000足す10000
+		System.out.println(ten + onehundred + onethousand 
+	          	           + tenthousand); 
+		
+		// 9.5足す10.5
 		System.out.println(f + d);
-		System.out.println(c + str + bool);
-		System.out.println(b + s + i + l + f + d);
-		System.out.println(b * s * i * l);
-		System.out.println(d / s);
-		System.out.println(b - s);
-
-		//5
+		
+		// 文字並べ
+		System.out.println(c + " " + str + " " + bool);
+		
+		// 数字を全て足す
+		System.out.println(ten + onehundred + onethousand
+		                   + tenthousand + f + d);
+		
+		// 少数点以外の数字を全てかける
+		System.out.println(ten * onehundred * onethousand
+				           * tenthousand);
+		
+		// 10.5割る100
+		System.out.println(d / onehundred);
+		
+		// 10引く100
+		System.out.println(ten - onehundred + "\n");
+	
+		
+		//5,次のプログラムを実行すると「ハローJAVA2023」という結果が表示されます。
+		//「ハローJAVA43」と表示とさせたいのですが、
+		//意図通りに動きません。正しく動作するように修正してください。
+		
+		System.out.println("問5");
+		
+		// 変数に20の値を代入
 		String num = "20";
+
+		// 変数に23を代入
 		int num1 = 23;
-		System.out.println("ハローJAVA" + (Integer.parseInt(num) + num1));
+		
+		// 変数の値を整数型に変換、num1と足した値を出力
+		System.out.println("ハローJAVA" + (Integer.parseInt(num) + num1 + "\n"));
+		
+		
+		//6,『』で囲われた人の情報を変数にして、
+		//formatの通りコンソールに出力してください
+		//ローカル変数に代入し○○に入れてください
 
-		//6
-		String info = "山田太郎 18歳 170.5cm 62.2kg 寿司";
-		String[] splitInfo = info.split(" ");
+		System.out.println("問6");
+		
+		
+		// 名前
+		String name = "山田太郎";
 
-		String name = splitInfo[0];
-		int age = Integer.parseInt(splitInfo[1].replace("歳", ""));
-		double height = Double.parseDouble(splitInfo[2].replace("cm", ""));
-		double weight = Double.parseDouble(splitInfo[3].replace("kg", ""));
-		String food = splitInfo[4];
+		// 年齢
+		int age = 18;
+		
+		// 身長
+		double height = 170.5;
+		
+		// 体重
+		double weight = 62.2;
 
-		System.out.println(String.format("初めまして%sです", name));
-		System.out.println(String.format("年齢は%d歳です", age));
-		System.out.println(String.format("身長は%.1fcmです", height));
-		System.out.println(String.format("体重は%.1fkgです", weight));
-		System.out.println(String.format("好きな食べ物は%sです", food));
+		// 好きな食べ物
+		String food = "寿司";
+		
+		// 名前を出力
+		System.out.println("初めまして" + name + "です");
+		
+		// 年齢を出力
+		System.out.println("年齢は" + age + "歳です");
+		
+		// 身長を出力
+		System.out.println("身長は" + height + "cmです");
+		
+		// 体重を出力
+		System.out.println("体重は" + weight + "kgです");
+		
+		// 好きな食べ物を出力
+		System.out.println("好きな食べ物は" + food + "です" + "\n");
+		
+		
+		//7,6で作成した自己紹介に続いてBMIが出力されるようにしてください
+		//ただし計算は数値を直書きせず、全て変数を使ってすること
+		
+		System.out.println("問7");
 
-		//7
+		
+		// BMIを計算
 		double heightInMeter = height / 100.0;
 		double bmi = weight / (heightInMeter * heightInMeter);
-		System.out.println(String.format("BMIは%.1fです", bmi));
 
-		//8
-		String name2 = "鈴木一郎";
-		int age2 = 24;
-		double height2 = 168.5;
-		double weight2 = 64.2;
-		String favoriteFood = "オムライス";
-		double BMI = weight2 / Math.pow(height2 / 100, 2);
-
-		System.out.println("初めまして" + name2 + "です");
-		System.out.println("年齢は" + age2 + "歳です");
-		System.out.println("身長は" + height2 + "cmです");
-		System.out.println("体重は" + weight2 + "kgです");
-		System.out.println("好きな食べ物は" + favoriteFood + "です");
-		System.out.println("BMIは" + String.format("%.2f", BMI) + "です");
-
-		//9 
-		String name21 = "鈴木一郎";
-		int age21 = 48;
-		double height21 = 337.0;
-		double weight21 = 128.4;
-		String favoriteFood1 = "オムライス";
-		double BMI1 = weight21 / Math.pow(height21 / 100, 2);
-
-		System.out.println("初めまして" + name21 + "です");
-		System.out.println("年齢は" + age21 + "歳です");
-		System.out.println("身長は" + String.format("%.1f", height21) + "cmです");
-		System.out.println("体重は" + String.format("%.1f", weight21) + "kgです");
-		System.out.println("好きな食べ物は" + favoriteFood1 + "です");
-		System.out.println("BMIは" + String.format("%.2f", BMI1) + "です");
+		// BMIを出力
+		System.out.println(String.format("BMIは%.1fです\n", bmi));
 		
-		//10
-		int age211 = 33;
-		System.out.println(age211 >= 25);
+		
+		//8,6で宣言した変数に再代入し下記の通りコンソールに出力してください
 
-		//11
-		String ageStr = String.valueOf(age2);
-		String heightStr = String.valueOf(height2);
-		String weightStr = String.valueOf(weight2);
+		System.out.println("問8");
+		
+		// 名前
+		name = "鈴木一郎";
+		
+		// 年齢
+		age = 24;
 
-		System.out.println("年齢は" + age2 + "歳です");
-		System.out.println("身長は" + height2 + "cmです");
-		System.out.println("体重は" + weight2 + "kgです");
+		// 身長
+		height = 168.5;
+		
+		// 体重
+		weight = 64.2;
+		
+		// 好きな食べ物
+		food = "オムライス";
+		
+		// BMI
+		bmi = 22.6;
+		
+		// 名前を出力
+		System.out.println("初めまして" + name + "です");
+		
+		// 年齢を出力
+		System.out.println("年齢は" + age + "歳です");
+		
+		// 身長を出力
+		System.out.println("身長は" + height + "cmです");
+		
+		// 体重を出力
+		System.out.println("体重は" + weight + "kgです");
+		
+		// 好きな食べ物を出力
+		System.out.println("好きな食べ物は" + food + "です");
+		
+		// BMIを出力
+		System.out.println("BMIは" +  bmi + "です" + "\n");
+		
+		
+		//9,8で使用した変数【年齢・身長・体重】の数値を和算で自己代入し、
+		//下記の通りコンソールに出力してください			
+        
+		System.out.println("問9");
+		
+		// 年齢
+		age = age + 24;
 
-		//12
-		System.out.println("年齢は" + age2 + "歳です");
-		System.out.println("身長は" + height2 + "cmです");
+		// 身長
+		height = height + 168.5;
 
-		//13
-		boolean result = ((age2 == 25 ? 1 : 0) | (height2 >= 160 ? 1 : 0)) == 1;
-		System.out.println(result);
+		// 体重
+		weight = weight + 64.2;
+
+		// BMI
+		bmi = weight / ((height / 100) * (height / 100));
+
+		// 名前を出力
+		System.out.println("初めまして" + name + "です");
+		
+		// 年齢を出力
+		System.out.println("年齢は" + age + "歳です");
+		
+		// 身長を出力
+		System.out.println("身長は" + String.format("%.1f", height) + "cmです");
+		
+		// 体重を出力
+		System.out.println("体重は" + String.format("%.1f", weight) + "kgです");
+
+		// 好きな食べ物を出力
+		System.out.println("好きな食べ物は" + food + "です");
+		
+		// BMIを出力
+		System.out.println("BMIは" + String.format("%.2f", bmi) + "です" + "\n");
+		
+		
+		//10,8で使用した年齢が25歳以上ならtrueが出力されるようにしてください。
+		//ただしif文は使いません
+
+		
+		System.out.println("問10");
+		
+		// 年齢を変数に代入
+		age = 24;
+
+		// 年齢が25歳以上かどうかを判断
+		System.out.println(age >= 25 ? "true\n" : "false\n");
+		
+		//11,8で使用した【年齢・身長・体重】を
+		//文字列型に型変換し繋げて出力してください
+        
+		System.out.println("問11");
+		
+		// 問題８の身長
+		height = 168.5;
+
+		
+		// 問題８の体重
+		weight = 64.2;
+
+		
+		// ageを文字列型に変換
+		String ageStr = String.valueOf(age);
+
+		// heightを文字列型に変換
+		String heightStr = String.valueOf(height);
+		
+		// weightを文字列型に変換
+		String weightStr = String.valueOf(weight);
+		
+		
+		// 繋げて出力
+		String result = "年齢は" + ageStr +  "歳です。" + " " +
+		                "身長は" + heightStr + "cmです。" +  " " +
+				        "体重は" + weightStr + "kgです。";
+				
+		// 結果を出力
+		System.out.println(result + "\n");
+
+		
+		//12,11で変換した【年齢・身長】を整数型に変換して出力してください
+
+		System.out.println("問12");
+				
+		// 年齢をint型に変換
+		int ageInt = Integer.parseInt(ageStr);
+
+        // 身長をint型に変換
+        int heightInt = (int) height;
+		
+		// 年齢を出力
+		System.out.println("年齢は" + ageInt + "歳です。");
+		
+		// 身長を出力
+        System.out.println("身長は" + heightInt + "cmです。" + "\n");
+		
+		
+		//13,12で変換した【年齢・身長】で
+		//【年齢が25もしくは身長が160以上】であればtrueを出力してください
+		//ただしif文は使わないでください
+
+		System.out.println("問13");
+		
+		
+		// 年齢が25際、または身長が160cm以上の場合
+		 boolean isConditionTrue = (ageInt == 25) || (heightInt >= 160);
+
+		
+		// result2の値を出力
+		System.out.println(result + "\n");
+
 	}
-
 }
