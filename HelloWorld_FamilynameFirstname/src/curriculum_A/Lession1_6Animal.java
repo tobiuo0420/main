94512511057935c28ddc6a13da011b6c49a2e08e
tobiuo0420 <133127590+tobiuo0420@users.noreply.github.com>
2023-07-02 10:29:13 +0900

    java課題　クラスの基本

--- HelloWorld_FamilynameFirstname/src/curriculum_A/Lession1_6Animal.java
+++ HelloWorld_FamilynameFirstname/src/curriculum_A/Lession1_6Animal.java
@@ -1,70 +1,63 @@
 package curriculum_A;
 
 import java.text.SimpleDateFormat;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 
 public class Lession1_6Animal {
+	// Animalクラス作成
+	static class Animal {
+		String name;
+		static int count = 0; // countフィールドを追加
 
-	// Dogクラスの定義
-	static class Dog {
-		String name = "ポチ";
-		static int count = 0;
+		// Q3：Q1で作成した変数に「犬」を代入するコンストラクタを作成してください。
+		public Animal(String name) {
+			this.name = name;
+			count++; 
+		}
 
-		public Dog() {
-			count++;
+		// Q4：Q2で作成した変数に引数を代入するコンストラクタを作成してください。
+		public Animal(int count) {
+			this.name = "動物";
+			Animal.count = count; 
+		}
+	}
+
+	static class Dog extends Animal {
+		public Dog(String name) {
+			super(name);
 		}
 	}
 
 	public static void main(String[] args) {
 
-		// Q1：Dogクラスを呼び出して変数に代入し、動物の名前をコンソールに出力してください。
+		// Q1：Animalクラスを呼び出して変数に代入し、動物の名前をコンソールに出力してください。
 		System.out.println("問1");
 
 		// インスタンスを生成し、名前を出力
-		Dog dog = new Dog();
+		Animal animal1 = new Animal("犬");
 
 		// 動物の名前をコンソールに出力
-		System.out.println(dog.name + "\n");
+		System.out.println(animal1.name + "\n");
 
-
-		// Q2：Dogクラスを呼び出して変数に代入し、動物の数をコンソールに出力してください。
+		// Q2：Animalクラスを呼び出して変数に代入し、動物の数をコンソールに出力してください。
 		System.out.println("問2");
 
-		// インスタンスを生成し、犬の数を出力
-		Dog dog1 = new Dog();
-
-		Dog dog2 = new Dog();
+		// インスタンスを生成し、動物の数を出力
+		Animal animal2 = new Animal(3);
 
 		// 動物の数をコンソールに出力
-		System.out.println(Dog.count + "\n");
+		System.out.println(Animal.count + "\n");
 
-		// 現在の日時を「yyyy-MM-dd H:m:s」形式で変数に代入して、コンソールに出力してください。
-
+		// Q3：現在の日時を「yyyy-MM-dd H:m:s」形式で変数に代入して、コンソールに出力してください。
 		System.out.println("問3");
 
-
 		// Calendarクラスのオブジェクトを生成
-		Calendar cl = Calendar.getInstance();
+		Calendar calendar = Calendar.getInstance();
 
 		// SimpleDateFormatクラスでフォーマットパターンを設定
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd H:m:s");
 
-		// 現在の日時を指定されたフォーマットで取得、
-		// コンソールに出力
-		System.out.println(sdf.format(cl.getTime()));
-
-		// DateTimeFormatterクラスのオブジェクトを生成
-		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
-
-		// String型の日付からLocalDateTimeクラスのオブジェクトを生成
-		String strDate = "2017/04/10 10:00:00";
-
-		LocalDateTime ld = LocalDateTime.parse(strDate, dtf);
-
-		// 指定されたフォーマットで日時を取得してコンソールに出力
-		System.out.println(ld.format(dtf).toString());
-
+		// 現在の日時を指定されたフォーマットで取得、コンソールに出力
+		System.out.println(sdf.format(calendar.getTime()));
 	}
 }
--- HelloWorld_FamilynameFirstname/src/curriculum_A/lession1_6.java
+++ HelloWorld_FamilynameFirstname/src/curriculum_A/lession1_6.java
@@ -6,8 +6,15 @@ public class lession1_6 {
 	static class Animal {
 		String name;
 
+		// Q3：Q1で作成した変数に「犬」を代入するコンストラクタを作成してください。
 		public Animal(String name) {
 			this.name = name;
+		}
+
+		// Q4：Q2で作成した変数に引数を代入するコンストラクタを作成してください。
+		public Animal(int count) {
+			this.name = "動物";
+
 		}
 	}
 
@@ -15,30 +22,16 @@ public class lession1_6 {
 
 		// Q1：フィールドに動物の名前の変数を定義してください。
 
+		String animalName;
 
 		// 犬を代入する変数 animalName を定義
-		String animalName = "犬";
-
+		animalName = "犬";
 
 		// Q2：フィールドに動物の数の変数を定義してください。
-
+		int animalCount;
 
 		// 動物の数を表す変数 animalCount を定義
-		int animalCount = 3;
-
-
-		// Q3：Q1で作成した変数に「犬」を代入するコンストラクタを作成してください。
-
-
-		// 犬を引数として持つ Animal オブジェクト dog を作成
-		Animal dog = new Animal("犬");
-
-
-		// Q4：Q2で作成した変数に引数を代入するコンストラクタを作成してください。
-
-
-		// 引数を持つ Animal オブジェクトを作成し、animalCount2 に代入
-		int animalCount2 = 3;
+		animalCount = 3;
 
 	}
 }
