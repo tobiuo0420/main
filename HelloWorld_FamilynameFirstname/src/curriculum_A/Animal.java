880d8010212eb37dc2ef3383c1f476dfffeb8c09
tobiuo0420 <133127590+tobiuo0420@users.noreply.github.com>
2023-07-05 22:27:04 +0900

    java課題　クラスの基本

---
+++ HelloWorld_FamilynameFirstname/src/curriculum_A/Animal.java
@@ -0,0 +1,22 @@
+package curriculum_A;
+
+public class Animal {
+	public static String count;
+
+	// Q1：フィールドに動物の名前の変数を定義してください。
+	String name;
+
+	// Q2：フィールドに動物の数の変数を定義してください。
+	int number;
+
+	// Q3：Q1で作成した変数に「犬」を代入するコンストラクタを作成してください。
+	public Animal(String name) {
+		this.name = name;
+
+	}
+
+	// Q4：Q2で作成した変数に引数を代入するコンストラクタを作成してください。
+	public Animal(int number) {
+		this.number = number;
+	}
+}
