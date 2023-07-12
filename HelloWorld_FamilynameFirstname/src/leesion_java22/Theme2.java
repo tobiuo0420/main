83ad066d13990e52b5e3035e6961771b7918e58d
tobiuo0420 <133127590+tobiuo0420@users.noreply.github.com>
2023-07-12 18:39:16 +0900

    java課題　パッケージ

---
+++ HelloWorld_FamilynameFirstname/src/leesion_java22/Theme2.java
@@ -0,0 +1,40 @@
+package leesion_java22;
+
+import leesion_java22_2.Sushi;
+
+public class Theme2 {
+
+	public static void main(String[] args) {
+		/*
+
+		下記がコンソールに出力されるように作成してください
+
+			こんにちは！ここは日本です！
+			この寿司はうまい
+			寿司は和食です
+			今の現在日時は2023/03/09 10:23:39です
+
+		【条件】
+			・JavaProjectを新規作成し、実行用のpackageと処理用のpackageを作成してください。
+			・適切なファイルにフィールドで変数を必要な数作ってください。
+			・thisを使って作成してください。
+			・日時は今日の日付を取得してください。
+
+		 */
+
+		// 日本の挨拶を表示
+		System.out.println("こんにちは！ここは日本です！");
+
+		// 寿司オブジェクトの作成と味の表示
+		Sushi sushi = new Sushi();
+		sushi.setTaste("うまい");
+		System.out.println("この寿司は" + sushi.getTaste());
+
+		// 寿司の種類を表示
+		System.out.println("寿司は" + Sushi.CUISINE + "です");
+
+		// 現在の日時を取得して表示
+		System.out.println("今の現在日時は" + sushi.getCurrentDateTime() + "です");
+
+	}
+}
---
+++ HelloWorld_FamilynameFirstname/src/leesion_java22_2/Sushi.java
@@ -0,0 +1,25 @@
+package leesion_java22_2;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class Sushi {
+
+	private String taste;
+	public static final String CUISINE = "和食";
+
+	public void setTaste(String taste) {
+		this.taste = taste;
+	}
+
+	public String getTaste() {
+		return taste;
+	}
+
+	public String getCurrentDateTime() {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		Date date = Calendar.getInstance().getTime();
+		return sdf.format(date);
+	}
+}
