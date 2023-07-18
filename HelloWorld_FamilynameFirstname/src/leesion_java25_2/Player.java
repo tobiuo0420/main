4c5d3d8c562df03a5b53a250a70ff99047f89691
tobiuo0420 <133127590+tobiuo0420@users.noreply.github.com>
2023-07-18 21:54:11 +0900

    java課題　継承の基本

---
+++ HelloWorld_FamilynameFirstname/src/leesion_java25_2/Player.java
@@ -0,0 +1,8 @@
+package leesion_java25_2;
+
+public class Player extends Person {
+
+	public Player(String name, int hp, int mp, int attack, int agility, int defense) {
+		super(name, hp, mp, attack, agility, defense);
+	}
+}
